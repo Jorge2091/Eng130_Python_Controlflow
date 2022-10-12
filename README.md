1d964bdd6c8989b5a34d5c9365338aa2ad7d275e
Jorge2091 <93310766+Jorge2091@users.noreply.github.com>
2022-10-12 16:11:30 +0100

    edit README.md for movie tickets

--- README.md
+++ README.md
@@ -16,3 +16,21 @@ if it's sunny: boolean value true - next line
 - let's go the beach
 
 else or elif
+
+### Age restriction for movie tickets
+
+with the if statement, you can make sure that all user can view or only select a potion of categories such as age restriction in movies<br/>
+We can use the `while` loop to keep asking for user input if the data doesn't match. This is to make sure the user inputs digits only
+```python
+promt = True
+while promt:
+    age = input("How old are you now? ")
+    if age.isdigit():
+        promt = False
+    else:
+        print("please input only digits")
+```
+We then need to make sure that the input is transformed from string to an integer value with the `int()` method.
+```python
+age = int(age)
+```
--- control_flow.py
+++ control_flow.py
@@ -27,11 +27,29 @@
 # 15 & above
 # if nothing matched, inform the user to enter their age again
 # user must not be allowed to enter age over 117 years
+promt = True
+while promt:
+    age = input("How old are you now? ")
+    if age.isdigit():
+        promt = False
+    else:
+        print("please input only digits")
 
-age = input("How old are you now?")
-if age > "117":
+age = int(age)
+
+if age > 117:
     print("please retry again and input correct value for age")
-elif age >"17":
+elif age >17:
     print("You can view any movie you like, no restriction on you")
-elif age > "15":
-    print("You can only view movies which are 16 and above")
+elif age > 15:
+    print("You can only view movies which are 16 and under")
+elif age > 14:
+    print("you can only view movies of 15 and under")
+elif age > 11:
+    print("you can view 12a movies and under")
+elif age > 7:
+    print("you can view pg movies and under")
+elif age > 3:
+    print("you can only watch U rated movies")
+else:
+    print("Too young")
