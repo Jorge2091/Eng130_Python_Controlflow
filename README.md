1693034946b61ffc35e1af12bed80c035c720325
Jorge2091 <93310766+Jorge2091@users.noreply.github.com>
2022-10-12 14:19:25 +0100

    add contrl flow py

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Python Control Flow
+
+## if elif else
+
+### Loops - for loop - while loop
+
+#### python keywords break - continue etc
+
+if it's cold:
+- take jacket
+
+if it's raining:
+- rain mack
+
+if it's sunny: boolean value true - next line
+- let's go the beach
+
+else or elif
---
+++ control_flow.py
@@ -0,0 +1,32 @@
+# pseudo coding
+
+# if it's cold:
+# - take jacket
+#
+# if it's raining:
+# - rain rack
+#
+# if it's sunny: boolean value true - next line
+# - let's go the beach
+#
+# else or elif
+# weather = "sunny"
+# if weather == "cold": #True or False
+#     print("wear a jacket")
+# elif weather == "sunny":
+#     print("let's go to the beach")
+# else:
+#     print("no match found")
+
+# age restriction for movie tickets
+# create a condition for 18 & above
+# 16 & above
+# universal 3 +
+# pg 8+
+# 12a
+# 15 & above
+# if nothing matched, inform the user to enter their age again
+# user must not be allowed to enter age over 117 years
+
+age = input("How old are you now?")
+if age > "117":
