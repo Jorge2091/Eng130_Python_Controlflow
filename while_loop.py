ffc94ca432bc8c59a02b117f81a44d7f8ed348e2
Jorge2091 <93310766+Jorge2091@users.noreply.github.com>
2022-10-12 15:26:48 +0100

    added year born calculator

--- while_loop.py
+++ while_loop.py
@@ -12,11 +12,16 @@
 
 user_prompt = True
 while user_prompt:
-    age = input("Please enter your age")
+    age = input("Please enter your age? ")
+
     if age.isdigit():
         user_prompt = False
     else:
         print("Please enter your age in digits only")
 print(f"Your age is {age}")
 
+
 # calculate their age - year of birth etc.
+year = 2022 - int(age)
+year = str(year)
+print("and you were born in {}".format(year))
